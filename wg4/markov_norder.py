4bab0c97ae7abf61073b8c2e55a2a0cf61bf6d04
Daniele Maccari <daniele.maccari@gmail.com>
2018-03-19 13:38:51 +0100

    Fixed hierarchy

--- wg4/markov_norder.py
+++ wg4/markov_norder.py
@@ -1,46 +1,57 @@
-import sys, random, collections, os
+import sys, random, collections, os, re
 
 # Since we split on whitespace, this can never be a word
 NONWORD = "\n"
 
 class Markov():
+    """
+    This basically parse the text through a fixed-size sliding window (the order param).
+    A dict stores a mapping between n-grams of size=order and the word coming right next.
+    At generation time, the dictionary is queried for possible words in the following loop:
+        - first, the most probable next word is chosen given the last seen n-gram
+        - second, this word is added to the list of seen word to generate the next n-gram.
+          This is equivalent to a one-step slide of the window.
+        - the loop is repeated
+    """
+    def __init__(self, order=2):
+        self.order = order
+        self.table = collections.defaultdict(list)
+        self.seen = collections.deque([NONWORD] * self.order, self.order)
 
-	def __init__(self, order=2):
-		self.order = order
-		self.table = collections.defaultdict(list)
-		self.seen = collections.deque([NONWORD] * self.order, self.order)
+    # Generate table
+    def generate_table(self, filename):
+        for line in open(filename):
+            for word in re.split(r' ', line):
+                if word != NONWORD:
+                    self.table[tuple(self.seen)].append(word)
+                    self.seen.append(word)
+        self.table[tuple(self.seen)].append(NONWORD)  # Mark the end of the file
 
-	# Generate table
-	def generate_table(self, filename):
-		for line in open(filename):
-			for word in line.split():
-				self.table[tuple(self.seen)].append(word)
-				self.seen.append(word)
-		self.table[tuple(self.seen)].append(NONWORD) # Mark the end of the file
+    # table, seen = generate_table("gk_papers.txt")
 
-	#table, seen = generate_table("gk_papers.txt")
+    # Generate output
+    def generate_output(self, max_words=100, newline_after=10):
+        self.seen.extend([NONWORD] * self.order)  # clear it all
+        for i in range(max_words):
+            word = random.choice(self.table[tuple(self.seen)])
+            if word == NONWORD:
+                exit()
+            if i % newline_after == 0:
+                print(word)
+            else:
+                print(word, end=' ')
+            self.seen.append(word)
 
-	# Generate output
-	def generate_output(self, max_words=100):
-		self.seen.extend([NONWORD] * self.order) # clear it all
-		for i in xrange(max_words):
-			word = random.choice(self.table[tuple(self.seen)])
-			if word == NONWORD:
-				exit()
-			print (word)
-			self.seen.append(word)
-
-
-	def walk_directory(self, rootDir):
-		for dirName, subdirList, fileList in os.walk(rootDir):
-			print('Found directory: %s' % dirName)
-			for fname in fileList:
-				self.generate_table(os.path.join(dirName,fname))
-				#print('\t%s' % fname)
+    def walk_directory(self, rootDir):
+        for dirName, subdirList, fileList in os.walk(rootDir):
+            print('Found directory: %s' % dirName)
+            for fname in fileList:
+                self.generate_table(os.path.join(dirName, fname))
+            # print('\t%s' % fname)
 
 
-m = Markov(order=3)
+m = Markov(order=5)
 
-#m.walk_directory('./pres-speech')
-m.walk_directory('./pres-speech/obama')
-m.generate_output(max_words=100)
+# m.walk_directory('./pres-speech')
+m.walk_directory('./pres-speech/clinton')
+m.generate_output(max_words=500)
